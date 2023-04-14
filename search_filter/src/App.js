8a1bf55e9c5d623f1034674d640dfdb5d26c6d09
poiu <94011063+poiudfg@users.noreply.github.com>
2023-04-14 12:22:07 +0700

    add table

--- search_filter/src/App.js
+++ search_filter/src/App.js
@@ -8,16 +8,34 @@ function App() {
     <div className="App">
       <h1>Search Filter with React</h1>
       <input type="text" placeholder="Search..." onChange={event =>{setSearchTerm(event.target.value)}} />
+      <table>
+            <tr>
+              <th>first name</th>
+              <th>last name</th>
+              <th>email</th>
+            </tr>
+            <tr className='space'>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
       {JSONDATA.filter((val) =>{
         if (searchTerm == ""){
           return val
-        }else if (val.first_name.toLocaleLowerCase().includes(searchTerm.toLowerCase())){
+        }else if (val.first_name.toLocaleLowerCase().includes(searchTerm.toLowerCase()) || 
+                  val.last_name.toLocaleLowerCase().includes(searchTerm.toLowerCase()) ||
+                  val.email.toLocaleLowerCase().includes(searchTerm.toLowerCase())){
           return val
         }}).map((val ,key) => {
         return <div className='user' key={key}>
-            <p>{val.first_name}</p>
+          <tr>
+            <td>{val.first_name} </td>
+            <td>{val.last_name} </td>
+            <td>{val.email}</td>
+          </tr>
           </div>
       })}
+      </table>
     </div>
   );
 }
